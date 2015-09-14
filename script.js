edffdb7b9599edf75e070b4e64796206952d685c
egmartinez98 <egmartinez98@gmail.com>
2015-09-14 11:55:19 -0700

    html and js file added

---
+++ script.js
@@ -0,0 +1,47 @@
+//step 1
+//var someMonth;
+//function theMonth
+//currentMonth
+//var summerMonth;
+//myLibraryFunction
+
+//step 2
+//5.5
+//"Hello World"
+//false
+//null
+
+//step 3
+//var basicMath = 5 + 5 (2 + 2);
+//var complexMath = basicMath + 100 * 100;
+
+//step 4
+//var firstName;
+//var lastName;
+//var address;
+//var city;
+//var state;
+//var zipCode;
+//var yourAge;
+//var referalSource;
+//var mayWeContactYou;
+
+//step 5
+
+//var firstName = "Emmanuel";
+//var lastName = "Martinez";
+//var address = "440 West san ysidro blvd";
+//var city = "San Diego";
+//var state = "CA";
+//var zipCode = 92173;
+//var yourAge = 17;
+//var referalSource = "Who referred you?";
+//var mayWeContactYou = true;
+
+//step 6
+//var myAge = 17;
+//console.log("Emmanuel Martinez is" + " " + myAge + " " + "Years old");
+
+var x = 10;
+var y = 10;
+console.log("x=y is a" + x == y);
