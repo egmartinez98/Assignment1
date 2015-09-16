6866ac53ff42263f9f75159381dcb1d5da4ebfca
Emmanuel <egmartinez98@gmail.com>
2015-09-16 14:56:28 -0700

    updated js file

---
+++ script.js
@@ -0,0 +1,99 @@
+//step 1
+//var someMonth;
+//function theMonth
+//currentMonth
+//var summerMonth;
+//myLibraryFunction
+
+//step 2
+//5.5
+//"Hello World"
+//false
+//null
+
+//step 3
+//var basicMath = 5 + 5 (2 + 2);
+//var complexMath = basicMath + 100 * 100;
+
+//step 4
+//var firstName;
+//var lastName;
+//var address;
+//var city;
+//var state;
+//var zipCode;
+//var yourAge;
+//var referalSource;
+//var mayWeContactYou;
+
+//step 5
+
+//var firstName = "Emmanuel";
+//var lastName = "Martinez";
+//var address = "440 West san ysidro blvd";
+//var city = "San Diego";
+//var state = "CA";
+//var zipCode = 92173;
+//var yourAge = 17;
+//var referalSource = "Who referred you?";
+//var mayWeContactYou = true;
+
+//step 6
+//var myAge = 17 + "Emmanuel Martinez";
+//console.log(myAge);
+
+//var _true =  true + "this is true";
+//console.log(true);
+
+//var bord = true + 25;
+//console.log(bord);
+
+//step 7
+//console.log(x);
+//var x = 5;
+
+//step 8
+//var someString = 'Who once said, "Only two things are infinite, the universe and human //stupidity, and I\'m not sure about the former."';
+//console.log(someString);
+
+//step 9
+//var x = null;
+//var y = undefined;
+
+//step 10
+//alert(typeof "Emmanuel");
+//alert(typeof 3.14);             
+//alert(typeof false);      
+//alert(typeof [1,2,3,4]);
+//aler(typeof undefined);
+
+//step 11
+//alert("Hello" + " " + "Emmanuel Martinez" + " " + ", welcome to the JavaScript class!");
+
+//step 12
+//var name = "Emmanuel Martinez";
+//alert("Hello" + " " + name + " " + ", welcome to the JavaScript class!");
+
+//step 13
+//var course = "JavaScript";
+//var name = "Emmanuel Martinez";
+//alert("Hello" + " " + name + " " + ", welcome to the" + " " + course + " " + "class!");
+
+//step 14
+//var name = "Emmanuel Martinez";
+//var course = "JavaScript";
+//alert("Hello" + " " + name + "." + " " + "\nWelcome to the" + " " + course + " " + "class!");
+
+//step 15
+//var name = prompt("Please enter your name.");
+//alert("Hello" + " " + name + "." + " " + "\nWelcome to the" + " " + "JavaScript class!");
+
+//step 16
+//var name = prompt("Please enter your name.");
+//var course = prompt("What course are you taking?");
+//alert("Hello" + " " + name + "." + " " + "\nWelcome to the" + " " + course + " " + "class!");
+
+//step 17
+var x = 10;
+var y = 20;
+console.log(x + y);
